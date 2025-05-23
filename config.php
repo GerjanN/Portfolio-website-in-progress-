29297f18bbb016b4350e47d262b01e11be87be8a
Gerjan <gerjan.nienhuis@gmail.com>
2025-05-23 13:29:53 +0200

    Fixed condig.php issue (for real this time)

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$_ENV['DB_HOST'] = 'localhost';
+$_ENV['DB_NAME'] = 'DB_NAME';
+$_ENV['DB_USER'] = 'DB_USER';
+$_ENV['DB_PASS'] = 'DB_PASS';
+
+$_ENV['LANG'] = 'nl';
+$_ENV['TITLE'] = 'Gerjan Portfolio Website';
